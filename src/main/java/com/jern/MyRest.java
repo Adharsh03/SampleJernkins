daa43e8b28a797966c234ce0b28e4765cba51ba9
Adharsh03 <kkadharsh2000@gmail.com>
2025-05-30 10:39:52 +0530

    new endpoints added

--- src/main/java/com/jern/MyRest.java
+++ src/main/java/com/jern/MyRest.java
@@ -1,6 +1,7 @@
 package com.jern;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,14 @@ public class MyRest {
 	@GetMapping(value="/hello")
 	public String getHello() {
 		return "Hello World";
+	}
+	
+	@GetMapping(value="/getMax/{num1}/{num2}")
+	public String getMaximum(@PathVariable int num1, @PathVariable int num2) {
+		if(num1>num2) {
+			return num1+" is maximum";
+		}else {
+			return num2+" is maximum";
+		}
 	}
 }
